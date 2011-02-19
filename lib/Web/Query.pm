029188be0d20634843f363e1652aad130adb7d56
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-19 10:38:39 +0900

    initial import

---
+++ lib/Web/Query.pm
@@ -0,0 +1,39 @@
+package Web::Query;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Web::Query -
+
+=head1 SYNOPSIS
+
+  use Web::Query;
+
+=head1 DESCRIPTION
+
+Web::Query is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
